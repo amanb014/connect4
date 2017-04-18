31a009e8d2adfc53e5ee7bd701a85f840e465ecc
Aman Bhimani <amanb014@gmail.com>
2017-04-18 17:19:08 -0400

    Added a base for the game logic. holders for most variables initialized to the default values.

--- js/main.js
+++ js/main.js
@@ -1,33 +1,73 @@
 //For defining hpw big the elements are, based on the window size. 
 var circleSize, columnSize, containerSize;
 
+var columnFilled, players;
+
 //DOM objects
 var boardContainer, columns;
 
 //constants
-const maxPiecesPerColumn = 6;
+const maxPerCol = 6;
+const dom = 0, width = 1, height = 2, piecesIn = 3;
 
 //DOM Objects
 boardContainer = document.getElementById('board-container');
+
+//initialize column array with default values from the page
 columns = new Array();
-
 for(var i = 0; i < 7; i++) {
-	columns.push(document.getElementById('column-' + i));
-	columns[i].addEventListener('click', addPiece);
+	columns.push({
+		DOM_Object: document.getElementById('column-' + i),
+		width: 		'width of container', 
+		height: 	'height of container',
+		physicalCount: 'number of pieces'
+	});
+	columns[i]['DOM_Object'].addEventListener('click', addPiece);
 }
-
 console.log(columns);
 
-//scoring system
+players = new Array();
+for(var i = 0; i < 2; i++) {
+	players.push({
+		name: 'Player ' + (i+1),
+		score: 0
+	});
+}
+console.log(players);
+
+columnFilled = [
+				[0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0]
+				];
+console.log(columnFilled);
+
+//Player system
 var p1 = {
 	name: 'Player 1',
 	score: 0
 }
 var p2 = {
 	name: 'Player 2',
-	score: 1
+	score: 0
 }
 
+// ADDING A PIECE TO THE GIVEN COLUMN.
 function addPiece(column) {
 	console.log('Adding piece to a column');
+}
+
+//Resets all scores, player names.
+//Basically refreshing the page
+function resetGame() {
+
+}
+
+//Plays again with the same players, 
+//keeping the scores intact.
+function playAgain() {
+
 }
