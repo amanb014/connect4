bb829b64598943bcb6d5e71b7b175c36197cb084
Aman Bhimani <amanb014@gmail.com>
2017-04-19 16:37:12 -0400

    Implemented the ball, change of column on hover (selection)

--- js/main.js
+++ js/main.js
@@ -1,63 +1,30 @@
 //For defining hpw big the elements are, based on the window size. 
-var circleSize, columnSize, containerSize;
+var circleSize, containerSize;
 
-var columnFilled, players;
+var columnFilled, players, activePlayer, activePiece;
 
 //DOM objects
-var boardContainer, columns;
+var boardContainer, columns; // columns is an array of them all
 
 //constants
 const maxPerCol = 6;
 const dom = 0, width = 1, height = 2, piecesIn = 3;
+const red = 1, black = 2;
 
-//DOM Objects
-boardContainer = document.getElementById('board-container');
+//initializes all important variables defined above. 
+//The function is at the very bottom.
+initVariables();
 
-//initialize column array with default values from the page
-columns = new Array();
-for(var i = 0; i < 7; i++) {
-	columns.push({
-		DOM_Object: document.getElementById('column-' + i),
-		width: 		'width of container', 
-		height: 	'height of container',
-		physicalCount: 'number of pieces'
-	});
-	columns[i]['DOM_Object'].addEventListener('click', addPiece);
-}
-console.log(columns);
-
-players = new Array();
-for(var i = 0; i < 2; i++) {
-	players.push({
-		name: 'Player ' + (i+1),
-		score: 0
-	});
-}
-console.log(players);
-
-columnFilled = [
-				[0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0]
-				];
-console.log(columnFilled);
-
-//Player system
-var p1 = {
-	name: 'Player 1',
-	score: 0
-}
-var p2 = {
-	name: 'Player 2',
-	score: 0
+function selectColumn(arg) {
+	console.log('Hovering over a column.. ');
+	activePiece.classList.remove('displayNone');
+	activePiece.style.margin = '0 0 0 ' + arg['DOM_Object'].offsetLeft + 'px';
 }
 
 // ADDING A PIECE TO THE GIVEN COLUMN.
-function addPiece(column) {
+function addPiece(arg) {
 	console.log('Adding piece to a column');
+	activePlayer = (activePlayer === 0) ? 1 : 0;
 }
 
 //Resets all scores, player names.
@@ -70,4 +37,59 @@ function resetGame() {
 //keeping the scores intact.
 function playAgain() {
 
+}
+
+function initVariables() {
+
+	//Data of columns
+	columns = new Array();
+	for(let i = 0; i < 7; i++) {
+		let dom = document.getElementById('column-' + i)
+		columns.push({
+			DOM_Object: dom,
+			id: 		'column-' + i,
+			count: 		0
+		});
+		columns[i]['DOM_Object'].addEventListener('click', function() {
+			addPiece(columns[i]);
+		});
+		columns[i]['DOM_Object'].addEventListener('mouseover', function() {
+			selectColumn(columns[i]);
+		});;
+	}
+
+	//Data for columns - 
+	// 0 = nothing in there
+	// 1 = red
+	// 2 = black
+	columnFilled = new Array();
+	for(let i = 0; i < 6; i++) {
+		columnFilled.push([0,0,0,0,0,0,0]);
+	}
+
+	activePiece = document.querySelector(".activePiece");
+	activePiece.classList.add('displayNone');
+	activePiece.style.width = columns[1]['DOM_Object'].offsetWidth + 'px';
+	activePiece.style.height = columns[1]['DOM_Object'].offsetWidth + 'px';
+
+	//DOM Variables
+	boardContainer = document.getElementById('board-container');
+	window.addEventListener('resize', function() {
+		containerSize = boardContainer.offsetWidth;
+		activePiece.style.width = columns[1]['DOM_Object'].offsetWidth + 'px';
+		activePiece.style.height = columns[1]['DOM_Object'].offsetWidth + 'px';
+	});
+	containerSize = boardContainer.offsetWidth;
+
+	//MISC
+	activePlayer = 0;
+
+	//Two player objects in a new array
+	players = new Array();
+		for(let i = 0; i < 2; i++) {
+			players.push({
+				name: 'Player ' + (i+1),
+				score: 0
+			});
+	}
 }
