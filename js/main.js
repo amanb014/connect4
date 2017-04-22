2679daccb942fe8c8ba079683dfc7377b4e8d925
Aman Bhimani <amanb014@gmail.com>
2017-04-22 12:25:01 -0400

    splitted initVariables function into several different ones. changed formatting

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-//For defining hpw big the elements are, based on the window size. 
-var circleSize, containerSize;
 
 var boardStatus, players, activePlayer, activePiece;
 
@@ -10,14 +8,64 @@ var boardContainer, columns; // columns is an array of them all
 const maxPerCol = 6;
 const dom = 0, width = 1, height = 2, piecesIn = 3;
 const p0 = 0, p1 = 1;
+var flag = true;
 
-//initializes all important variables defined above. 
-//The function is at the very bottom.
-initVariables();
+window.onload = function() {
+	columns = [];
+	for(let i = 0; i < 7; i++) {
+		let dom = document.getElementById('column-' + i)
+		columns.push({
+			DOM_Object: dom,
+			id: 		'column-' + i,
+			count: 		0,
+			ind: 		i
+		});
+		columns[i]['DOM_Object'].addEventListener('click', function() {
+			addPiece(columns[i]);
+		});
+		columns[i]['DOM_Object'].addEventListener('mouseover', function() {
+			selectColumn(columns[i]);
+		});;
+	}
+
+	//DOM Variables
+	boardContainer = document.getElementById('board-container');
+	activePiece = document.querySelector('.activePiece');
+
+	//Window Listeners
+	window.addEventListener('resize', function() {
+		containerSize = boardContainer.offsetWidth;
+		activePiece.style.width = columns[1]['DOM_Object'].offsetWidth + 'px';
+		activePiece.style.height = columns[1]['DOM_Object'].offsetWidth + 'px';
+
+		console.log((columns[1]['DOM_Object'].offsetHeight / 6) + 'px');
+	});
+
+	activePiece.classList.add('player-'+activePlayer);
+	activePiece.style.width = columns[1]['DOM_Object'].offsetWidth + 'px';
+	activePiece.style.height = columns[1]['DOM_Object'].offsetWidth + 'px';
+
+	//Two player objects in a new array
+	players = [];
+		for(let i = 0; i < 2; i++) {
+			players.push({
+				name: 'Player ' + (i+1),
+				score: 0,
+				pieces: 0
+			});
+	}
+
+	resetBoardData();
+	playAgain();
+}
 
 function selectColumn(col) {
 	console.log('Hovering over a column.. ');
-	activePiece.classList.remove('displayNone');
+
+	if(flag) {
+		activePiece.classList.remove('displayNone');
+		flag = false;
+	}
 	activePiece.style.margin = '0 0 0 ' + col['DOM_Object'].offsetLeft + 'px';
 }
 
@@ -28,7 +76,8 @@ function addPiece(col) {
 		boardStatus[5 - col['count']][col['ind']] = activePlayer;
 		addPieceToUI(5 - col['count'], col['ind']);
 		col['count']++;
-		console.log(boardStatus);
+		players[activePlayer].pieces++;
+		// console.log(boardStatus);
 		nextPlayer();
 	} 
 	else {
@@ -48,18 +97,32 @@ function addPieceToUI(row, col) {
 
 }
 
-//Resets all scores, player names.
-//Basically refreshing the page
-function resetGame() {
-
-}
-
 //Plays again with the same players, 
 //keeping the scores intact.
 function playAgain() {
 
+	//first player is the new active player
+	activePlayer = p0;
+
+	//reset the count of pieces in each column. 
+	//reset the UI inside each column
+	for(let i = 0; i < 7; i++) {
+		columns[i]['count'] = 0;
+		columns[i]['DOM_Object'].innerHTML = '';
+	}
+
+	for(let i = 0; i < 2; i++) {
+		players[i].score = 0;
+		players[i].pieces = 0;
+	}
 }
 
+//Change the name of players
+function changeNames() {
+
+}
+
+//Chooses the next player after each turn
 function nextPlayer() {
 
 	let next = (activePlayer === p0) ? p1 : p0;
@@ -71,70 +134,13 @@ function nextPlayer() {
 	activePlayer = next;
 }
 
-function initVariables() {
-
-	//Data of columns
-	columns = new Array();
-	for(let i = 0; i < 7; i++) {
-		let dom = document.getElementById('column-' + i)
-		columns.push({
-			DOM_Object: dom,
-			id: 		'column-' + i,
-			count: 		0,
-			ind: 		i
-		});
-		columns[i]['DOM_Object'].addEventListener('click', function() {
-			addPiece(columns[i]);
-		});
-		columns[i]['DOM_Object'].addEventListener('mouseover', function() {
-			selectColumn(columns[i]);
-		});;
-	}
-
+function resetBoardData() {
 	//Data for columns - 
 	// false = nothing in there
 	// 0 = red
 	// 1 = black
-	boardStatus = new Array();
+	boardStatus = [];
 	for(let i = 0; i < 6; i++) {
 		boardStatus.push([false,false,false,false,false,false,false]);
-	}
-
-	activePlayer = p0;
-
-	activePiece = document.querySelector(".activePiece");
-	activePiece.classList.add('displayNone', 'player-'+activePlayer);
-		activePiece.style.width = columns[1]['DOM_Object'].offsetWidth + 'px';
-		activePiece.style.height = columns[1]['DOM_Object'].offsetWidth + 'px';
-
-	//DOM Variables
-	boardContainer = document.getElementById('board-container');
-	window.addEventListener('resize', function() {
-		containerSize = boardContainer.offsetWidth;
-		activePiece.style.width = columns[1]['DOM_Object'].offsetWidth + 'px';
-		activePiece.style.height = columns[1]['DOM_Object'].offsetWidth + 'px';
-
-		// console.log(document.querySelectorAll('.placedPiece').offsetWidth);
-
-
-		// document.querySelectorAll('.placedPiece').style.width = (columns[1]['DOM_Object'].offsetHeight / 6) + 'px';
-		// document.querySelectorAll('.placedPiece').style.height = (columns[1]['DOM_Object'].offsetHeight / 6) + 'px';
-
-		console.log((columns[1]['DOM_Object'].offsetHeight / 6) + 'px');
-	});
-	containerSize = boardContainer.offsetWidth;
-
-	//Two player objects in a new array
-	players = new Array();
-		for(let i = 0; i < 2; i++) {
-			players.push({
-				name: 'Player ' + (i+1),
-				score: 0
-			});
-	}
-
-	function resetPieceSizes() {
-
-
 	}
 }
